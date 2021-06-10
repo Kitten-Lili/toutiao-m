990d1bd9590711f0dbf1738cca7888e7849226ca
jlc <1760470603@qq.com>
2021-06-10 15:44:04 +0800

    项目初始化-引入Vant组件

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
+import Vant from 'vant'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载Vant全局样式
+import 'vant/lib/index.css'
+
 // 加载全局样式
 import './styles/index.less'
+
+// 注册Vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
